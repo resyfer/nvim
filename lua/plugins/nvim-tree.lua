f3dad1d57c9ca307434e5454d4630cb1fcb05733
Saurav Pal <resyfer.dev@gmail.com>
2025-01-27 10:22:00 +0530

    Nvim Tree file focus

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -4,6 +4,18 @@ return {
     "nvim-tree/nvim-web-devicons",
   },
   opts = {
+    actions = {
+      open_file = {
+        resize_window = true, -- Resize the window when opening a file
+        window_picker = {
+          enable = true, -- Allow window picker to choose the window
+        },
+      },
+    },
+    update_focused_file = {
+      enable = true, -- Automatically focus on the file when it's opened
+      update_cwd = true, -- Update the current working directory when focusing
+    },
     sort = {
       sorter = "case_sensitive",
     },
